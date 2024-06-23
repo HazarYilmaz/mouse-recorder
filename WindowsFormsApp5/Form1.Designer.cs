42d41b16c43a72ff1b999d209767f44da24a7683
Hazar Yılmaz <143789273+HazarYilmaz@users.noreply.github.com>
2024-06-24 02:11:45 +0300

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class mouse_player
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mouse_player));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ReplayButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.RecordButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(159)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 52);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::WindowsFormsApp5.Properties.Resources.cross;
+            this.button1.Location = new System.Drawing.Point(449, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 46);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ReplayButton
+            // 
+            this.ReplayButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ReplayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ReplayButton.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.ReplayButton.ForeColor = System.Drawing.Color.White;
+            this.ReplayButton.Image = global::WindowsFormsApp5.Properties.Resources.play_button_arrowhead;
+            this.ReplayButton.Location = new System.Drawing.Point(357, 95);
+            this.ReplayButton.Name = "ReplayButton";
+            this.ReplayButton.Size = new System.Drawing.Size(117, 100);
+            this.ReplayButton.TabIndex = 2;
+            this.ReplayButton.Text = "Başlat";
+            this.ReplayButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ReplayButton.UseVisualStyleBackColor = false;
+            this.ReplayButton.Click += new System.EventHandler(this.ReplayButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.StopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StopButton.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.StopButton.Image = global::WindowsFormsApp5.Properties.Resources.stop_button;
+            this.StopButton.Location = new System.Drawing.Point(190, 95);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(117, 100);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Durdur";
+            this.StopButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.StopButton.UseVisualStyleBackColor = false;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.RecordButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RecordButton.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.RecordButton.ForeColor = System.Drawing.Color.White;
+            this.RecordButton.Image = global::WindowsFormsApp5.Properties.Resources.button;
+            this.RecordButton.Location = new System.Drawing.Point(21, 95);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(117, 100);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.Text = "Kayıt";
+            this.RecordButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.RecordButton.UseVisualStyleBackColor = false;
+            this.RecordButton.Click += new System.EventHandler(this.RecordButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(414, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mouse Player";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // mouse_player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 251);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ReplayButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.RecordButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mouse_player";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ReplayButton;
+        private System.Windows.Forms.Button RecordButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class mouse_player : Form
+    {
+        private List<(int x, int y, int delay)> recordedActions;
+        private bool isRecording;
+        public mouse_player()
+        {
+            InitializeComponent();
+            recordedActions = new List<(int, int, int)>();
+            isRecording = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void RecordButton_Click(object sender, EventArgs e)
+        {
+            recordedActions.Clear();
+            isRecording = true;
+            MessageBox.Show("Kayıt başladı. İşlemlerinizi gerçekleştirin.");
+
+            // Mouse hook to record actions
+            MouseHook.Start(this);
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            isRecording = false;
+            MessageBox.Show("Kayıt Durduruldu");
+
+            // Unhook mouse hook after recording
+            MouseHook.Stop();
+        }
+
+        private void ReplayButton_Click(object sender, EventArgs e)
+        {
+            if (recordedActions.Count == 0)
+            {
+                MessageBox.Show("Tekrar oynatılacak kayıtlı eylem yok.");
+                return;
+            }
+
+            MessageBox.Show("Kayıt Başlatılıyor Lütfen Fareye müdahalede bulunmayın");
+            
+            // Replay recorded actions
+            foreach (var action in recordedActions)
+            {
+                Thread.Sleep(1000); // Bekleme süresi ekle
+                Cursor.Position = new System.Drawing.Point(action.x, action.y);
+                InputSimulator.MouseLeftButtonClick(); // Simulate left mouse button click
+            }
+            this.BringToFront();
+            SystemSounds.Beep.Play();
+            MessageBox.Show("Kayıt Bitti");
+
+        }
+        private void baslat()
+        {
+            if (recordedActions.Count == 0)
+            {
+                MessageBox.Show("Tekrar oynatılacak kayıtlı eylem yok.");
+                return;
+            }
+
+            MessageBox.Show("Kayıt Başlatılıyor Lütfen Fareye müdahalede bulunmayın");
+
+            // Replay recorded actions
+            foreach (var action in recordedActions)
+            {
+                Thread.Sleep(1000); // Bekleme süresi ekle
+                Cursor.Position = new System.Drawing.Point(action.x, action.y);
+                InputSimulator.MouseLeftButtonClick(); // Simulate left mouse button click
+            }
+            MessageBox.Show("Kayıt Bitti");
+        }
+        private static class MouseHook
+        {
+            private static LowLevelMouseProc _proc;
+            private static IntPtr _hookID;
+            private static mouse_player _formInstance;
+
+            public static void Start(mouse_player formInstance)
+            {
+                _formInstance = formInstance;
+                _proc = HookCallback;
+                _hookID = SetHook(_proc);
+            }
+
+            public static void Stop()
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+
+            private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+            private static IntPtr SetHook(LowLevelMouseProc proc)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                        GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+
+            private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if (nCode >= 0 && wParam == (IntPtr)WM_LBUTTONDOWN)
+                {
+                    MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                    if (_formInstance != null && _formInstance.isRecording)
+                    {
+                        _formInstance.recordedActions.Add((hookStruct.pt.x, hookStruct.pt.y, 0)); // Record mouse click
+                    }
+                }
+                return CallNextHookEx(_hookID, nCode, wParam, lParam);
+            }
+
+            private const int WH_MOUSE_LL = 14;
+            private const int WM_LBUTTONDOWN = 0x0201;
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct POINT
+            {
+                public int x;
+                public int y;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct MSLLHOOKSTRUCT
+            {
+                public POINT pt;
+                public uint mouseData;
+                public uint flags;
+                public uint time;
+                public IntPtr dwExtraInfo;
+            }
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // F2 tuşuna basıldığında ReplayButton_Click metodunu çağır
+            if (e.KeyCode == Keys.F2)
+            {
+                e.Handled = true; // Olayın işlendiğini belirt
+                baslat();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            formTasiniyor = true;
+            baslangicNoktasi = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (formTasiniyor)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.baslangicNoktasi.X, p.Y - this.baslangicNoktasi.Y);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            formTasiniyor = false;
+        }
+        bool formTasiniyor = false;
+        Point baslangicNoktasi = new Point(0, 0);
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            formTasiniyor = true;
+            baslangicNoktasi = new Point(e.X, e.Y);
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            formTasiniyor = false;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (formTasiniyor)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.baslangicNoktasi.X, p.Y - this.baslangicNoktasi.Y);
+            }
+        }
+    }
+
+    // Input simulator class to simulate input events
+    public static class InputSimulator
+    {
+        public static void MouseLeftButtonClick()
+        {
+            INPUT[] inputs = new INPUT[2];
+
+            inputs[0] = new INPUT();
+            inputs[0].type = InputType.MOUSE;
+            inputs[0].u.mi.dx = 0;
+            inputs[0].u.mi.dy = 0;
+            inputs[0].u.mi.mouseData = 0;
+            inputs[0].u.mi.dwFlags = MouseEventFlags.MOUSEEVENTF_LEFTDOWN;
+
+            inputs[1] = new INPUT();
+            inputs[1].type = InputType.MOUSE;
+            inputs[1].u.mi.dx = 0;
+            inputs[1].u.mi.dy = 0;
+            inputs[1].u.mi.mouseData = 0;
+            inputs[1].u.mi.dwFlags = MouseEventFlags.MOUSEEVENTF_LEFTUP;
+
+            SendInput(2, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            public InputType type;
+            public MouseKeybdhardwareInputUnion u;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct MouseKeybdhardwareInputUnion
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
+        }
+
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public MouseEventFlags dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        public enum InputType : uint
+        {
+            MOUSE = 0,
+            KEYBOARD = 1,
+            HARDWARE = 2
+        }
+
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            MOUSEEVENTF_MOVE = 0x0001,
+            MOUSEEVENTF_LEFTDOWN = 0x0002,
+            MOUSEEVENTF_LEFTUP = 0x0004,
+            MOUSEEVENTF_RIGHTDOWN = 0x0008,
+            MOUSEEVENTF_RIGHTUP = 0x0010,
+            MOUSEEVENTF_MIDDLEDOWN = 0x0020,
+            MOUSEEVENTF_MIDDLEUP = 0x0040,
+            MOUSEEVENTF_XDOWN = 0x0080,
+            MOUSEEVENTF_XUP = 0x0100,
+            MOUSEEVENTF_WHEEL = 0x0800,
+            MOUSEEVENTF_VIRTUALDESK = 0x4000,
+            MOUSEEVENTF_ABSOLUTE = 0x8000
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+    }
+}
